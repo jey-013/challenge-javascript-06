0fda09eed34068897bb7294c99d6b5b145dadab6
Jesus Mejia <chucho4720013@gmail.com>
2020-07-02 00:33:11 -0500

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -7,9 +7,32 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+const combinationSumRecursive = (candidates, target) => {
+  //Declarar
+  let results = []
+  let temp = new Array()
+  const reducer = (accumulator, curentValue) => accumulator + curentValue;
+  for (let i = 0; i < candidates.length; i++) {
+    let condition = true
+    while (condition) {
+      temp.push(candidates[i])
+      
+      if (temp.reduce(reducer, 0) === target) {
+        let drop = new Array()
+        drop = Array.from(temp)
+        results.push(drop)
+        condition = false
+      }
+      
+      if (temp.reduce(reducer, 0) > target) {
+        if (temp[0] == candidates[i]) condition = false
+        if (temp.length > 0) temp.pop()
+        temp.splice(0,1)
+      }
+    }
   }
+  return results
+}
   
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
